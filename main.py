3ddafc0e3efa56901f8c4df932aa04eed3dcc9f8
George Tsetsegmaa <macdev@macdev-2.local>
2026-02-28 17:52:21 -0800

    Add verbose argument to output more details

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from google.genai import types
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -18,10 +19,13 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    generate_content(client=client, messages=messages)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+
+    generate_content(client, messages, args.verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
@@ -29,8 +33,9 @@ def generate_content(client, messages):
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
     
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.promp_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
 
 
