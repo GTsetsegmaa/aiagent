3ba721906c5a114639c89f2177c99dff56327e79
George Tsetsegmaa <macdev@macdev-2.local>
2026-02-28 17:27:26 -0800

    Add messages storing

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY evniornment variable not set")
 
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client=client, messages=messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
