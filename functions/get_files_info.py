a947b5e2f56ca711725a617f085ec44d6b2631d6
George Tsetsegmaa <macdev@macdev-2.local>
2026-04-17 16:03:36 -0700

    Change function signature

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,6 @@
 import os
 
-def get_files_info(working_directory: str, directory=".") -> str:
+def get_files_info(working_directory, directory="."):
     try:
         working_dir_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
